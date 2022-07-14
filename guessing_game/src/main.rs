79bd4dc0fd6401a6592e3220d3d6b5eaa97ce842
Steven Bias <bias_steven@hotmail.fr>
2022-07-14 09:42:47 +0200

    Scan and print user number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;        // Input io library
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please, input your guess: ");
+
+    let mut guess = String::new();           // Mutable variable
+    io::stdin().read_line(&mut guess)       // &mut guess instead of &guess to make it mutable
+        .expect("Failed to read line");     // Catch error
+
+    println!("You guessed: {}", guess);
 }
