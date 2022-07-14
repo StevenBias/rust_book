5dfe9de64619686412661f638886a9514c8622b0
Steven Bias <bias_steven@hotmail.fr>
2022-07-14 10:16:14 +0200

    Print random number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,15 @@
 use std::io;        // Input io library
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
     println!("Please, input your guess: ");
 
+    let secret_number = rand::thread_rng().gen_range(0, 101);   // gen_range function is inclusive on the lower bond but exclusive on the upper bond
+
     let mut guess = String::new();           // Mutable variable
     io::stdin().read_line(&mut guess)       // &mut guess instead of &guess to make it mutable
         .expect("Failed to read line");     // Catch error
 
-    println!("You guessed: {}", guess);
+    println!("The secret number is: {}", secret_number);
 }
