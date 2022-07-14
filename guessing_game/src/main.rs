a5f6b67b9da53ebe72a031ae0ab7fb288f28f943
Steven Bias <bias_steven@hotmail.fr>
2022-07-14 11:00:00 +0200

    Add a loop to have several try

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,27 +4,33 @@ use std::cmp::Ordering;     // For number comparison
 
 fn main() {
     println!("Guess the number!");
-    println!("Please, input your guess: ");
 
     // gen_range function is inclusive on the lower bond but exclusive on the upper bond
     let secret_number = rand::thread_rng().gen_range(0, 101);
 
-    let mut guess = String::new();          // Mutable variable
-    io::stdin().read_line(&mut guess)       // &mut guess instead of &guess to make it mutable
-        .expect("Failed to read line");     // Catch error
+    loop {
+        println!("Please, input your guess: ");
 
-    /*  Shadow previous string guess variable
-     *
-     *  trim    remove whitespaces at the bebining and the end of a string
-     *  parse   parse string into number, catch an error in case the string does not contain number
-     */
-    let guess : u8 = guess.trim().parse()
-        .expect("Please type a number");
+        let mut guess = String::new();          // Mutable variable
+        io::stdin().read_line(&mut guess)       // &mut guess instead of &guess to make it mutable
+            .expect("Failed to read line");     // Catch error
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less    => println!("It's higher"),
-        Ordering::Greater => println!("It's lower"),
-        Ordering::Equal   => println!("You win!"),
+        /*  Shadow previous string guess variable
+         *
+         *  trim    remove whitespaces at the bebining and the end of a string
+         *  parse   parse string into number, catch an error in case the string does not contain number
+         */
+        let guess : u8 = guess.trim().parse()
+            .expect("Please type a number");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("It's higher"),
+            Ordering::Greater => println!("It's lower"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            }
+        }
     }
     println!("The secret number is: {}", secret_number);
 }
