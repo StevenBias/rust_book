082c01a382c19568d6457b6d20a3a59013577cd5
Steven Bias <bias_steven@hotmail.fr>
2022-07-14 10:51:00 +0200

    Compare number once ok

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,16 +6,25 @@ fn main() {
     println!("Guess the number!");
     println!("Please, input your guess: ");
 
-    let secret_number = rand::thread_rng().gen_range(0, 101);   // gen_range function is inclusive on the lower bond but exclusive on the upper bond
+    // gen_range function is inclusive on the lower bond but exclusive on the upper bond
+    let secret_number = rand::thread_rng().gen_range(0, 101);
 
-    let mut guess = String::new();           // Mutable variable
+    let mut guess = String::new();          // Mutable variable
     io::stdin().read_line(&mut guess)       // &mut guess instead of &guess to make it mutable
         .expect("Failed to read line");     // Catch error
 
+    /*  Shadow previous string guess variable
+     *
+     *  trim    remove whitespaces at the bebining and the end of a string
+     *  parse   parse string into number, catch an error in case the string does not contain number
+     */
+    let guess : u8 = guess.trim().parse()
+        .expect("Please type a number");
+
     match guess.cmp(&secret_number) {
-        Ordering::Less    => println!("It's higher");
-        Ordering::Greater => println!("It's lower");
-        Ordering::Equal   => println!("You win!");
+        Ordering::Less    => println!("It's higher"),
+        Ordering::Greater => println!("It's lower"),
+        Ordering::Equal   => println!("You win!"),
     }
     println!("The secret number is: {}", secret_number);
 }
