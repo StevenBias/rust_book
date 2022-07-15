389f4dde9bc2a69884f385f2c4b3c5e9d7970ae4
Steven Bias <bias_steven@hotmail.fr>
2022-07-15 09:56:43 +0200

    If condition

---
+++ branches/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let number= 3;
+
+    // Do not use parenthesis for condition...
+    if number < 5 {
+        println!("Condition was true");
+    } else {
+        println!("Condition was false");
+    }
+}
