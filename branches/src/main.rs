d765b6e6e288861bd9a5f3d446eac82bb16e0f6c
Steven Bias <bias_steven@hotmail.fr>
2022-07-15 10:01:04 +0200

    Using if statement in let statement

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -2,9 +2,18 @@ fn main() {
     let number= 3;
 
     // Do not use parenthesis for condition...
+    // The condition MUST be a bool
     if number < 5 {
         println!("Condition was true");
     } else {
         println!("Condition was false");
     }
+
+    let condition = false;
+    let number = if condition {
+        5
+    } else {
+        6
+    };
+    println!("The value of number is: {}", number);
 }
