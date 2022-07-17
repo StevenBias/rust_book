3b977fc2dd685ae98e3ee148280ac9cedc7de184
Steven Bias <bias_steven@hotmail.fr>
2022-07-17 18:50:45 +0200

    Copy stack-only data

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
+    /******     Variables in heap       ******/
+    //  Strings are variables with unknown size, so it's stored in heap
+    //  When the data is "moved", it is no more valid
+
     // "from" method make a mutable variable
     let s1 = String::from("Hello");      // s1 is valid from this point forward
     let s2 = s1;                        // s1 is "moved" to s2
@@ -11,4 +15,15 @@ fn main() {
     let s4 = s3.clone();
 
     println!("s3 = {}, s4 = {}", s3, s4);
+
+    /******     Variables in heap       ******/
+    //  All "primitives" types have known size
+    //  So they are stored in stack
+    //  When they are "moved", the variable is still valid
+    //  There is no difference between deep and shallow copying
+    //  So no need to use the "clone" method
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
 }
