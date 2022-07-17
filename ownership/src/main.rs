92099b9ee0d567b1e622a743dc89f2391b601afe
Steven Bias <bias_steven@hotmail.fr>
2022-07-17 18:40:39 +0200

    Clone

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
+    // "from" method make a mutable variable
     let s1 = String::from("Hello");      // s1 is valid from this point forward
     let s2 = s1;                        // s1 is "moved" to s2
 
     // println!("{}, world", s1);          // s1 is no more valid
     println!("{}, world", s2);          // But s2 is stil in the scope
+
+    /***    Do a deep copy with clone   ***/
+    let s3 = String::from("Clone test");
+    let s4 = s3.clone();
+
+    println!("s3 = {}, s4 = {}", s3, s4);
 }
