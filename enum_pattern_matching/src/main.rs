7a03d289d78c7448d3076572dcd3bfa20a19fbec
Steven Bias <bias_steven@hotmail.fr>
2022-07-24 21:22:14 +0200

    Matching with Option<T>

--- enum_pattern_matching/src/main.rs
+++ enum_pattern_matching/src/main.rs
@@ -52,7 +52,16 @@ fn main() {
     let state = UsState::State1;
     let coin = Coin::Quarter(state);
     value_in_cents(coin);
+
+    // Matching with Option<T>
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("The value of six is: {:?}", six);
+    println!("The value of none is: {:?}", none);
 }
+
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
         Coin::Penny     => 1,
@@ -65,3 +74,9 @@ fn value_in_cents(coin: Coin) -> u8 {
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1)
+    }
+}
