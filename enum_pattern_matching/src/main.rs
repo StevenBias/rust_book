afad5abc33fef3fa2c08df7ceea914d15112293f
Steven Bias <bias_steven@hotmail.fr>
2022-07-24 20:42:10 +0200

    Null values

--- enum_pattern_matching/src/main.rs
+++ enum_pattern_matching/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    // Null values
+    let set_number = Some(5);
+    let set_string = Some("toto");
+    let absent_number: Option<i32> = None;
 }
