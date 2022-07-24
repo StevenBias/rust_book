ddba62726c18f0be1244c8589ec1e5efa517b477
Steven Bias <bias_steven@hotmail.fr>
2022-07-24 21:14:55 +0200

    Match and pattern that bind values

--- enum_pattern_matching/src/main.rs
+++ enum_pattern_matching/src/main.rs
@@ -1,12 +1,32 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+#[derive(Debug)]
+enum UsState {
+    State0,
+    State1,
+    State2,
+    /****/
+}
+
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
-    enum IpAddrKind {
-        V4,
-        V6,
-    }
-    struct IpAddr {
-        kind: IpAddrKind,
-        address: String,
-    }
     let home = IpAddr {
         kind: IpAddrKind::V4,
         address: String::from("127.0.0.1"),
@@ -16,8 +36,32 @@ fn main() {
         address: String::from("::1"),
     };
 
+    println!("Home value: {:?}", home);
+    println!("loopback value: {:?}", loopback);
+
     // Null values
     let set_number = Some(5);
     let set_string = Some("toto");
     let absent_number: Option<i32> = None;
+
+    println!("set_number value: {:?}", set_number);
+    println!("set_string value: {:?}", set_string);
+    println!("absent_number value: {:?}", absent_number);
+
+    // The match Control Flow Operator
+    let state = UsState::State1;
+    let coin = Coin::Quarter(state);
+    value_in_cents(coin);
 }
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny     => 1,
+        Coin::Nickel    => 5,
+        Coin::Dime      => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        }
+    }
+}
+
