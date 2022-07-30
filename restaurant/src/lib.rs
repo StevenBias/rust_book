b68484533ae13c077eb2888cda8d6fd87994a2c8
Steven Bias <bias_steven@hotmail.fr>
2022-07-30 21:45:31 +0200

    Separating Modules into Different Files

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,14 +1,6 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_at_table() {}
-    }
-    pub mod serving {
-        fn take_order() {}
-        pub fn serve_order() {}
-        fn take_payment() {}
-    }
-}
+// Tells Rust to load the contents of the module from another file with the
+// same name as the module
+mod front_of_house;
 
 mod back_of_house {
     pub enum Appetizer {
