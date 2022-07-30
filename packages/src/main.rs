e68b175733442a781bbdb522eba1bb65de4ceaf3
Steven Bias <bias_steven@hotmail.fr>
2022-07-30 22:50:56 +0200

    Use restaurant library in package project ok

--- packages/src/main.rs
+++ packages/src/main.rs
@@ -10,14 +10,23 @@ use std::io::Result as IoResult;
 // The foolowing is equivalent to:
 // use std::io;
 // use std::io::Write;
-use std::io::{self, Write};
+// use std::io::{self, Write};
 
 // Bring all public items from std::collections
-use std::collections::*;
+// use std::collections::*;
+
+use restaurant::eat_at_restaurant;
 
 fn main() {
     let mut map = HashMap::new();
     map.insert(1, 2);
+    let a = function1();
+    let b = function2();
+    println!("{:?}", a);
+    println!("{:?}", b);
+    println!("{:?}", map);
+
+    eat_at_restaurant();
 }
 
 fn function1() -> Result {
