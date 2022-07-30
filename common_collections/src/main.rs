6ef6693ed8e7da141c0998355dbeab7d7f156717
Steven Bias <bias_steven@hotmail.fr>
2022-07-30 23:45:44 +0200

    Update a vector

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    let v1: Vec<i32> = Vec::new();
+    let mut v1: Vec<i32> = Vec::new();
     let v2 = vec![1, 2, 3];
+
+    v1.push(5);
+    v1.push(6);
+    v1.push(7);
+    v1.push(8);
+
+    println!("{:?}", v1);
+    println!("{:?}", v2);
 }
