4eafee16aaa5ff2281a061dd72b4e83d7513e276
Steven Bias <bias_steven@hotmail.fr>
2022-07-30 20:08:31 +0200

    Provide new name with the as keyword

--- packages/src/main.rs
+++ packages/src/main.rs
@@ -1,5 +1,18 @@
 use std::collections::HashMap;
+use std::fmt::Result;
+use std::io::Result as IoResult;
+
 fn main() {
     let mut map = HashMap::new();
     map.insert(1, 2);
+}
+
+fn function1() -> Result {
+    /**/
+    Ok(())
+}
+
+fn function2() -> IoResult<()> {
+    /**/
+    Ok(())
 }
