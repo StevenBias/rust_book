c84ab70293c5ba6bb2e3313e5c98725027337bc9
Steven Bias <bias_steven@hotmail.fr>
2022-08-02 01:47:21 +0200

    WIP implement merge sort algo

--- common_collections/exo1/src/main.rs
+++ common_collections/exo1/src/main.rs
@@ -1,9 +1,28 @@
 fn main() {
-    let v = vec![10, 20];
+    let v = vec![10, 20, 5];
+
     let mut mean = 0;
     for i in &v {
         mean += i;
     };
     mean /= v.len();
     println!("Mean is: {}", mean);
+}
+
+fn mergeSort(vector: &mut Vec<i32>, left: u32, right: u32) {
+    if left > right {
+        let middle = left + (left - right)/2;
+        mergeSort(vector, left, middle);
+        mergeSort(vector, middle+1, right);
+        merge(vector, left, middle, right);
+    }
+}
+
+fn merge(vector: &mut Vec<i32>, left: u32, middle: u32, right: u32) {
+    let mut index = left;
+    let mut indLeft = 0;
+    let mut indRight = 0;
+    // Create two distinct sub-slices from a slice:
+    let (vectorLeft, vectorRight) = vector.split_at_mut(middle.try_into().unwrap());     // Use try_into().unwrap() to convert a 'u32' to a 'usize' and panic if the converted value doesn't fit!
+
 }
