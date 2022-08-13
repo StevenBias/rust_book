f6dff9d4c190e1c272be9aaaea0022116fd4f263
Steven Bias <bias_steven@hotmail.fr>
2022-08-14 00:17:36 +0200

    Shortcut panic on error with unwrap

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,14 +1,5 @@
 use std::fs::File;
-use std::io::ErrorKind;
 
 fn main() {
-    let _f = File::open("hello.txt").unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
-                panic!("Problem creating the file: {:?}", error);
-            })
-        } else {
-            panic!("Problem openin the file: {:?}", error);
-        }
-    });
+    let _f = File::open("hello.txt").unwrap();
 }
