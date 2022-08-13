798f898ed2491a187a6d1ec6bcb509d18e5b33f5
Steven Bias <bias_steven@hotmail.fr>
2022-08-14 01:23:51 +0200

    Shorten the read from file

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,19 +1,15 @@
 use std::io;
-use std::io::Read;
-use std::fs::File;
+use std::fs;
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut f = File::open("hello.txt")?;
-    let mut s = String::new();
-    f.read_to_string(&mut s)?;
-    return Ok(s);
+    return fs::read_to_string("hello.txt");
 }
 
 fn main() {
-    let _f = read_username_from_file();
+    let _username = read_username_from_file();
 
-    let _f = match _f {
-        Ok(file) => file,
+    let _username = match _username {
+        Ok(s) => println!("The username is: {}", s),
         Err(e) => panic!("Error: {:?}", e),
     };
 }
