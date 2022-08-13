be5c4616dd6404c0a3379077349ce9784f70de24
Steven Bias <bias_steven@hotmail.fr>
2022-08-14 01:13:10 +0200

    Use of '?' to propagade error

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,5 +1,19 @@
+use std::io;
+use std::io::Read;
 use std::fs::File;
 
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    return Ok(s);
+}
+
 fn main() {
-    let _f = File::open("hello.txt").expect("Failed to open file");
+    let _f = read_username_from_file();
+
+    let _f = match _f {
+        Ok(file) => file,
+        Err(e) => panic!("Error: {:?}", e),
+    };
 }
