199d4c3b5e1731f70316befa4aba6e3fb754413b
Steven Bias <bias_steven@hotmail.fr>
2022-08-14 18:02:57 +0200

    Add custom failure message

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -6,6 +6,8 @@ mod tests {
     use super::*;
 #[test]
     fn it_adds_two() {
-        assert_eq!(4, add_two(2));
+        let res = 4;
+        assert_eq!(res, add_two(2),
+        "The result is false, it must be {} instead of {}", res, add_two(2));
     }
 }
