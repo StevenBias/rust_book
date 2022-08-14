bc37e71a9e5f3fa916269fdc69335eb2dd62cf99
Steven Bias <bias_steven@hotmail.fr>
2022-08-14 10:27:57 +0200

    WIP type T

--- generic_type/src/main.rs
+++ generic_type/src/main.rs
@@ -1,4 +1,4 @@
-fn largest_i32(list: &[i32]) -> i32 {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
     for &item in list.iter() {
         if item > largest {
@@ -7,20 +7,12 @@ fn largest_i32(list: &[i32]) -> i32 {
     }
     largest
 }
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
-}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
     let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 }
