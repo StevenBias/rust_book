ae8d767bddae5c52654b4b89d3e08c2e5025d05e
Steven Bias <bias_steven@hotmail.fr>
2022-08-14 02:04:16 +0200

    Use '?' operator only with function that returns Result

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 use std::fs;
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    return fs::read_to_string("hello.txt");
+    Ok(fs::read_to_string("hello.txt")?)
 }
 
 fn main() {
