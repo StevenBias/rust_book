fbf074eae3b28aee73b06572c0fb3edbdeacc2cb
Steven Bias <bias_steven@hotmail.fr>
2022-08-14 16:06:00 +0200

    Use trait bound to conditionnaly implement methods

--- generic_type/src/main.rs
+++ generic_type/src/main.rs
@@ -20,6 +20,28 @@ impl<T, U> Point<T, U> {
     }
 }
 
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+impl<T> Pair<T> {
+    fn new(x: T, y:T) -> Self {
+        Self {
+            x,
+            y,
+        }
+    }
+}
+impl<T: Display + Debug + PartialOrd> Pair<T> {
+    fn cmd_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x");
+        } else {
+            println!("The largest member is y");
+        }
+    }
+}
+
 fn largest<T: PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
     let mut index = 0;
@@ -33,12 +55,12 @@ fn largest<T: PartialOrd>(list: &[T]) -> &T {
 }
 
 /****   Trait bounds with where Clauses  ****/
-fn some_function<T, U>(t: T, u: U) -> i32
-    where T: Display + Clone,
-          U: Clone + Debug
-{
-    42
-}
+// fn some_function<T, U>(t: T, u: U) -> i32
+//     where T: Display + Clone,
+//           U: Clone + Debug
+// {
+//     42
+// }
 
 /****   Return types that implements trait  ****/
 fn returns_summarizable() -> impl Summary {
@@ -88,4 +110,7 @@ fn main() {
 
     let ret_trait = returns_summarizable();
     println!("Test return trait type: {}", ret_trait.summarize());
+
+    let pair = Pair::new(5, 8);
+    pair.cmd_display();
 }
