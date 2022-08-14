6a7af4cab9d5456e5983da85b1477c61cf77b45e
Steven Bias <bias_steven@hotmail.fr>
2022-08-15 01:08:33 +0200

    Move code from main file to lib file

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,25 @@
+use std::error::Error;
+use std::fs;
+
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() > 2 {
+            Ok(Config { query: args[1].clone(), filename: args[2].clone() })
+        } else {
+            return Err("There are not enough arguments")
+        }
+    }
+}
+
+// dyn Error => Dynamix error to not avoid to declare the type of error
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let content = fs::read_to_string(config.filename)?;
+    println!("With text:\n{}", content);
+    Ok(())
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,7 @@
-use std::error::Error;
 use std::process;
 use std::env;
-use std::fs;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,28 +14,5 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config).expect("Error while reading file");
-
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() > 2 {
-            Ok(Config { query: args[1].clone(), filename: args[2].clone() })
-        } else {
-            return Err("There are not enough arguments")
-        }
-    }
-}
-
-// dyn Error => Dynamix error to not avoid to declare the type of error
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let content = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", content);
-    Ok(())
+    minigrep::run(config).expect("Error while reading file");
 }
