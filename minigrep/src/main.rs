41aa95b0dd78cf6686ad49cdea52e28069374d6c
Steven Bias <bias_steven@hotmail.fr>
2022-08-15 00:43:13 +0200

    WIP

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,20 +4,15 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let num = args.len();
-    if num > 2 {
-        let config = Config::new(&args);
+    let config = Config::new(&args);
 
-        println!("Searching for {}", config.query);
-        println!("In file {}", config.filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-        let content = fs::read_to_string(config.filename)
-            .expect("Error while reading file");
+    let content = fs::read_to_string(config.filename)
+        .expect("Error while reading file");
 
-        println!("With text:\n{}", content);
-    } else {
-        panic!("There are not enough arguments");
-    }
+    println!("With text:\n{}", content);
 }
 
 struct Config {
@@ -27,6 +22,10 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Config {
-        Config { query: args[1].clone(), filename: args[2].clone() }
+        if args.len() > 2 {
+            Config { query: args[1].clone(), filename: args[2].clone() }
+        } else {
+            panic!("There are not enough arguments");
+        }
     }
 }
