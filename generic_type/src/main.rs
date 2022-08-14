d58e92e9e9b06c1e6f3e52850f74470135be8b7b
Steven Bias <bias_steven@hotmail.fr>
2022-08-14 13:21:20 +0200

    Use trait bound for the 'largest' function

--- generic_type/src/main.rs
+++ generic_type/src/main.rs
@@ -20,15 +20,15 @@ impl<T, U> Point<T, U> {
     }
 }
 
-// fn largest<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
-//     for &item in list.iter() {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-//     largest
-// }
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
 
 /****   Trait bounds with where Clauses  ****/
 fn some_function<T, U>(t: T, u: U) -> i32
@@ -52,12 +52,12 @@ fn notify<T: Summary>(item: T) {
 }
 
 fn main() {
-    // let number_list = vec![34, 50, 25, 100, 65];
-    // let result = largest(&number_list);
-    // println!("The largest number is {}", result);
-    // let char_list = vec!['y', 'm', 'a', 'q'];
-    // let result = largest(&char_list);
-    // println!("The largest char is {}", result);
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 
     let p0 = Point { x: 5, y: 10};
     let p1 = Point { x: 1.0, y: 4.0};
