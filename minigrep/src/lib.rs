c0c4d64afa3733dfa46e76ef033c1be02485794d
Steven Bias <bias_steven@hotmail.fr>
2022-08-19 23:10:53 +0200

    Use iterator adopter for search function

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -52,13 +52,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 // Use en explicit lifetime 'a to specify that this is the content argument lifetime
 // which is connected to the return Vec
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut res = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            res.push(line);
-        }
-    }
-    return res;
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
