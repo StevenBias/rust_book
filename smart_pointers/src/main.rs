cfbc09254f8c9f78d8ba4b126424d9d9984b622e
Steven Bias <bias_steven@hotmail.fr>
2022-08-22 21:08:54 +0200

    Test counting reference

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -61,6 +61,12 @@ fn main() {
     test_drop();
 
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
