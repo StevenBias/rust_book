04e1ba57ff90cb699545770fe67516c53010fc8d
Steven Bias <bias_steven@hotmail.fr>
2022-08-31 14:20:26 +0200

    Sending Multiple Values and Seeing the Receiver Waiting
    In this example rx is treating as an iterator
    so the recv function is not called explicitly

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::sync::mpsc;
+use std::time::Duration;
 
 fn threads() {
     let v = vec![1, 2, 3];
@@ -17,11 +18,21 @@ fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
     // recv method is blocking, whereas try_recv is not
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    // let received = rx.recv().unwrap();
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
