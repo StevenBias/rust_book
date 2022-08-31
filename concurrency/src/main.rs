188295e9af7e5debb708391f2c64be1a0f82dd8c
Steven Bias <bias_steven@hotmail.fr>
2022-08-31 14:51:16 +0200

    Mutex API

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use std::sync::mpsc;
 use std::time::Duration;
+use std::sync::Mutex;
 
 fn threads() {
     let v = vec![1, 2, 3];
@@ -12,9 +13,7 @@ fn threads() {
     handle.join().unwrap();
 }
 
-fn main() {
-    threads();
-
+fn messages() {
     let (tx, rx) = mpsc::channel();
 
     let tx1 = mpsc::Sender::clone(&tx);
@@ -50,4 +49,23 @@ fn main() {
     for received in rx {
         println!("Got: {}", received);
     }
+}
+
+fn main() {
+    // threads();
+    // messages();
+
+    // Create Mutex<i32>
+    // Mutex is a smart pointer
+    let m = Mutex::new(5);
+
+    {
+        // We have to lock the mutex before access to the data
+        // lock method return a MutexGuard
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    // As Mutex is a smart pointer, the lock is dropped when the MutexGuard is out of scope
+
+    println!("m = {:?}", m);
 }
