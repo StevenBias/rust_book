998b880bce16c69e72f82d65d32e7b2de5f4bd95
Steven Bias <bias_steven@hotmail.fr>
2022-09-06 05:35:10 +0200

    while let Conditional Loops

---
+++ patterns/src/main.rs
@@ -0,0 +1,15 @@
+fn while_let() {
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+}
+
+fn main() {
+    while_let();
+}
