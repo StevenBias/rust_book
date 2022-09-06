9d75bb3f8fad01a3171e9642d2fde677b74e5fcb
Steven Bias <bias_steven@hotmail.fr>
2022-09-06 10:17:47 +0200

    Using @ lets us test a value and save it in a variable within one pattern.

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -81,6 +81,27 @@ fn ignoring_pattern() {
     }
 }
 
+fn binding() {
+    println!("\n@ Binding");
+    enum Message {
+        Hello {id: i32},
+    }
+
+    let msg = Message::Hello{id: 5};
+
+    match msg {
+        Message::Hello{id: id_variable @ 3..=7} => {
+            println!("Found an id in range: {}", id_variable)
+        },
+        Message::Hello{id: 10..=12} => {
+            println!("Found an id in another range")
+        },
+        Message::Hello{ id } => {
+            println!("Found some other id: {}", id)
+        }
+    }
+}
+
 fn main() {
     while_let();
     for_loop();
@@ -88,4 +109,5 @@ fn main() {
     pattern_syntax();
     destrucure_struct();
     ignoring_pattern();
+    binding();
 }
