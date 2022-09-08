f5658919702463a91dc28487b919e1d0451fd74f
Steven Bias <bias_steven@hotmail.fr>
2022-09-08 13:50:41 +0200

    Creating a safe abstraction over unsafe code

--- advanced_features/src/main.rs
+++ advanced_features/src/main.rs
@@ -1,7 +1,15 @@
+use std::slice;
+
 fn arbitrary_mem_add() {
     println!("Creating a raw pointer to an arbitrary memory address");
     let address = 0x012345usize;
-    let r = address as *const i32;
+    let r = address as *mut i32;
+
+    // We have no guarantee this code is safe
+    // bc we don't own the memory at this arbitrary location
+    let slice: &[i32] = unsafe {
+        slice::from_raw_parts_mut(r, 10000)
+    };
 }
 
 unsafe fn raw_pointers() {
@@ -16,10 +24,28 @@ unsafe fn raw_pointers() {
     println!("r2 is: {}", *r2);
 }
 
-fn main() {
-    arbitrary_mem_add();
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
 
     unsafe {
-        raw_pointers();
+        (slice::from_raw_parts_mut(ptr, mid),
+         slice::from_raw_parts_mut(ptr.offset(mid as isize), len - mid))
     }
+}
+
+fn main() {
+    arbitrary_mem_add();
+    unsafe { raw_pointers();}
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
 }
