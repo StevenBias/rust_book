229c3fb9ea1d56ec3724b490a75a22d1f78c12a4
Steven Bias <bias_steven@hotmail.fr>
2022-09-12 00:23:10 +0200

    Validate the request and selectively respond

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -18,18 +18,18 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    println!("Request: {:#?}", http_request);
-    
-    let status_line = "HTTP/1.1 200 OK\r\n\r\n";       // 200 is a status code for success response
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let mut status_line = "HTTP/1.1 200 OK\r\n\r\n"; // 200 is a status code for success response
+    let mut filename = String::from("hello.html");
+
+    if request_line != "GET / HTTP/1.1" {
+        status_line = "HTTP/1.1 404 NOT FOUND";
+        filename = String::from("404.html");
+    }
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
-
     let response =
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
